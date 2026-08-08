50f101d59e50e79477b15107ad2a11cf6964cd6d
CorneliusCorn <jrosenbeckerdev@gmail.com>
2026-08-08 01:02:40 -0500

    Cleanup and rename

--- src/main/java/corneliuscorn/stellaritelib/StellariteLib.java
+++ src/main/java/corneliuscorn/stellaritelib/StellariteLib.java
@@ -1,17 +1,17 @@
-package com.example.stellaritelib;
+package corneliuscorn.stellaritelib;
 
-import com.example.stellaritelib.registry.ModBlocks;
-import com.example.stellaritelib.registry.ModItems;
-import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
+import corneliuscorn.stellaritelib.registry.ModBlocks;
+import corneliuscorn.stellaritelib.registry.ModItems;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
 
 @Mod(StellariteLib.MOD_ID)
 public final class StellariteLib {
     public static final String MOD_ID = "stellarite_lib";
     public static final Logger LOGGER = LogUtils.getLogger();
-
+    
     public StellariteLib(IEventBus modEventBus) {
         ModBlocks.BLOCKS.register(modEventBus);
         ModItems.register(modEventBus);
--- src/main/java/corneliuscorn/stellaritelib/registry/ModItems.java
+++ src/main/java/corneliuscorn/stellaritelib/registry/ModItems.java
@@ -1,6 +1,6 @@
-package com.example.stellaritelib.registry;
+package corneliuscorn.stellaritelib.registry;
 
-import com.example.stellaritelib.StellariteLib;
+import corneliuscorn.stellaritelib.StellariteLib;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
@@ -10,13 +10,27 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public final class ModItems {
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(StellariteLib.MOD_ID);
+    public static final DeferredRegister.Items ITEMS =
+        DeferredRegister.createItems(StellariteLib.MOD_ID);
 
-    public static final DeferredItem<BlockItem> MOON_TEST_ORE = ITEMS.registerSimpleBlockItem(ModBlocks.MOON_TEST_ORE);
-    public static final DeferredItem<BlockItem> RAW_MOON_TEST_ORE_BLOCK = ITEMS.registerSimpleBlockItem(ModBlocks.RAW_MOON_TEST_ORE_BLOCK);
-    public static final DeferredItem<BlockItem> MOON_TEST_ORE_BLOCK = ITEMS.registerSimpleBlockItem(ModBlocks.MOON_TEST_ORE_BLOCK);
-    public static final DeferredItem<Item> RAW_MOON_TEST_ORE = ITEMS.registerSimpleItem("raw_moon_test_ore");
-    public static final DeferredItem<Item> MOON_TEST_ORE_INGOT = ITEMS.registerSimpleItem("moon_test_ore_ingot");
+    public static final DeferredItem<BlockItem> MOON_TEST_ORE = ITEMS.registerSimpleBlockItem(
+        ModBlocks.MOON_TEST_ORE);
+    
+    public static final DeferredItem<BlockItem> RAW_MOON_TEST_ORE_BLOCK =
+        ITEMS.registerSimpleBlockItem(
+        ModBlocks.RAW_MOON_TEST_ORE_BLOCK);
+
+    public static final DeferredItem<BlockItem> MOON_TEST_ORE_BLOCK = ITEMS.registerSimpleBlockItem(
+        ModBlocks.MOON_TEST_ORE_BLOCK);
+
+    public static final DeferredItem<Item> RAW_MOON_TEST_ORE = ITEMS.registerSimpleItem(
+        "raw_moon_test_ore");
+
+    public static final DeferredItem<Item> MOON_TEST_ORE_INGOT = ITEMS.registerSimpleItem(
+        "moon_test_ore_ingot");
+
+    private ModItems() {
+    }
 
     public static void register(IEventBus modEventBus) {
         ITEMS.register(modEventBus);
@@ -33,6 +47,4 @@ public final class ModItems {
             event.accept(MOON_TEST_ORE_BLOCK);
         }
     }
-
-    private ModItems() {}
 }
